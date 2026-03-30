8f5c6568d96d21d572b5a236cd852daad6c61498
Your Name <your_email@example.com>
2026-03-30 20:53:32 +0100

    Fix deps: remove sanity studio, add .npmrc for peer deps

--- src/sanity/schemaTypes/index.ts
+++
@@ -1,8 +0,0 @@
-import { type SchemaTypeDefinition } from "sanity";
-import { categoryType } from "./categoryType";
-import { productType } from "./productType";
-import { orderType } from "./orderType";
-
-export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [categoryType, productType, orderType],
-};
