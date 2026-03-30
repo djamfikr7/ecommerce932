ffcbb24f5586291539e071cda8ed0186aca9d8fe
Your Name <your_email@example.com>
2026-03-30 21:23:02 +0100

    Hardcode Sanity project ID for Vercel build

--- src/sanity/lib/client.ts
+++ src/sanity/lib/client.ts
@@ -1,9 +1,8 @@
 import { createClient } from "next-sanity";
-import { apiVersion, dataset, projectId } from "../env";
 
 export const client = createClient({
-  projectId: projectId || "wllinfoz",
-  dataset,
-  apiVersion,
+  projectId: "wllinfoz",
+  dataset: "production",
+  apiVersion: "2024-12-01",
   useCdn: true,
 });
